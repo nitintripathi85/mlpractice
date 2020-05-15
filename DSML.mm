8613222706e6be85e4d00a8b0dfc66a2886799db
Nitin Tripathi <nitin.tripathi85@gmail.com>
2020-05-15 10:51:40 +0400

    commit

---
+++ DSML.mm
@@ -0,0 +1,9 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#ff0000" CREATED="1589475616050" ID="ID_1754146743" MODIFIED="1589479551218" TEXT="DS/ML">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1589475633572" ID="ID_899996877" MODIFIED="1589479555732" POSITION="right" TEXT="Statistics">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
